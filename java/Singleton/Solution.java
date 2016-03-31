df5fd8fc0d2dc340e88c8f0997e168944414b1cf
Tao Lin <lintaog3@gmail.com>
2016-03-31 15:48:39 +0200

    2016-03-31 update singleton. dont know how to do oo question. copy from other.

---
+++ java/Singleton/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @return: The same instance of this class every time
+     */
+    private Solution(){
+         
+    }
+    private static  volatile Solution instance = new Solution();
+    
+    public static Solution getInstance() {
+        // write your code here
+        return instance ;
+    }
+};
